9028ab48432d87983925cebc26672cf6af582671
Linda Chen Gray <linda167@gmail.com>
2018-12-15 14:56:56 -0800

    Initial commit of project

---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+public class CardScript : MonoBehaviour {
+	// Time to hover before showing full card
+	private const int hoverTimeBeforeShowingFullCardMS = 700;
+
+	// Time to animate showing full card
+	private const int showFullCardAnimationTimeMS = 150;
+
+	
+	private const float cardMoveSpeed = 11f;
+
+	// Initial scale to set the full card for animation
+	private Vector3 showFullCardAnimationInitialScale = new Vector3(0.75f, 0.75f, 0.75f);
+
+	[SerializeField]
+	private GameManagerScript gameManager;
+
+	[SerializeField]
+	private Transform selectionBorder;
+
+	[SerializeField]
+	private Text attackValueDisplay;
+
+	[SerializeField]
+	private Text healthValueDisplay;
+
+	[SerializeField]
+	private Text damageValueDisplay;
+
+	[SerializeField]
+	private Canvas canvas;
+
+	[SerializeField]
+	private GameObject fullCard;
+
+	[SerializeField]
+	private GameObject damageIndicator;
+
+	[SerializeField]
+	private bool isEnemyPlayer;
+
+	// Current attack and health value
+	private int attackValue;
+	private int healthValue;
+
+	// Whether this card is selected
+	private bool isSelected = false;
+
+	// Time when the mouse entered the card, null if mouse is not currently over card
+	private DateTime mouseEnterTime;
+
+	private Vector2 originalPosition;
+
+	private Vector2 targetPosition = Vector2.zero;
+	
+	// Time when we started showing the full card
+	private DateTime fullCardShowStartTime;
+
+	public bool IsSelected {
+		get { return this.isSelected; }
+	}
+
+	public bool IsEnemyPlayer {
+		get { return this.isEnemyPlayer; }
+	}
+
+	public int AttackValue {
+		get { return this.attackValue; }
+	}
+
+	public int HealthValue {
+		get { return this.healthValue; }
+	}
+
+	private bool IsFullCardShown {
+		get { return this.fullCard.GetComponent<Renderer>().enabled; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		// Initialize random attack / health values
+		this.attackValue = UnityEngine.Random.Range(1,6);
+		this.healthValue = UnityEngine.Random.Range(2,10);
+
+		// Hide selection border to start
+		selectionBorder.GetComponent<Renderer>().enabled = false;
+
+		// Set full card to be the highest sorting layer
+		this.fullCard.GetComponent<Renderer>().sortingOrder = 10;
+
+		if (this.isEnemyPlayer) {
+			// Enemy cards don't move for now
+			Destroy(GetComponent<Rigidbody2D>());
+		}
+
+		this.originalPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Update movement if we're moving towards target
+		this.UpdateMoveTowardsTarget();
+
+		// Update whether to show full card
+		this.UpdateFullCardDisplay();
+
+		// Make sure rotation is straight
+		this.transform.rotation = Quaternion.identity;
+
+		// Update attack and health values
+		attackValueDisplay.text = attackValue.ToString();
+		healthValueDisplay.text = healthValue.ToString();
+	}
+
+	private void UpdateMoveTowardsTarget() {
+		// We're in the process of moving towards a target position
+		if (this.targetPosition != Vector2.zero) {
+			float step = cardMoveSpeed * Time.deltaTime;
+			this.transform.position = Vector2.MoveTowards(this.transform.position, this.targetPosition, step);
+
+			// We've reached our destination
+			if (this.transform.position.Equals(this.targetPosition)) {
+				this.targetPosition = Vector2.zero;
+
+				// If we've returned to original position, reset layer to default
+				if (this.transform.position.Equals(this.originalPosition))  {
+					this.gameManager.onAttackFinished();
+				}
+			}
+		}
+	}
+
+	private void UpdateFullCardDisplay() {
+		// If attack in progress, close full card
+		if (this.gameManager.IsPlayersTurn &&
+			this.gameManager.IsAttackInProgress) {
+			this.closeFullCard();
+		}
+
+		// Animate showing full card
+		if (!this.fullCardShowStartTime.Equals(default(DateTime))) {
+			double timeSinceStartAnimation = DateTime.Now.Subtract(this.fullCardShowStartTime).TotalMilliseconds;
+			if (timeSinceStartAnimation >= CardScript.showFullCardAnimationTimeMS) {
+				// Animation is done
+				this.fullCard.transform.localScale = new Vector3(1, 1, 1);
+				this.fullCardShowStartTime = default(DateTime);
+			} else {
+				float initialScale = this.showFullCardAnimationInitialScale.x;
+				float newScale = (float)(initialScale + 
+					(timeSinceStartAnimation / (float) CardScript.showFullCardAnimationTimeMS) *
+					(1 - initialScale));
+				this.fullCard.transform.localScale = new Vector3(newScale, newScale, newScale);
+			}
+		}
+	}
+
+	void OnMouseDown() {
+		this.gameManager.OnCardClicked(this.gameObject);
+    }
+
+	void OnMouseEnter() {
+		this.mouseEnterTime = DateTime.Now;
+	}
+
+	void OnMouseOver() {
+		if (!this.IsFullCardShown &&
+			!(this.gameManager.IsPlayersTurn &&
+			this.gameManager.IsAttackInProgress) &&
+			!this.mouseEnterTime.Equals(default(DateTime)) &&
+			DateTime.Now.Subtract(this.mouseEnterTime).TotalMilliseconds > hoverTimeBeforeShowingFullCardMS) {
+			
+			// Show full card on long hover
+			this.startShowFullCard();
+		}
+	}
+
+	void OnMouseExit() {
+		this.mouseEnterTime = default(DateTime);
+		this.closeFullCard();
+    }
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (this.isEnemyPlayer) {
+			// Enemies don't move
+			return;
+		}
+
+		if (other.gameObject.tag == "Card") {
+			CardScript cardBeingHitScript = other.gameObject.GetComponent<CardScript>();
+
+			// Enemy card being hit should be the only one on this layer
+			Assert.IsTrue(cardBeingHitScript.IsEnemyPlayer);
+
+			// We've hit the enemy
+			this.gameManager.OnCardsCollide();
+		}
+	}
+
+	public void SelectCard() {
+		this.isSelected = true;
+	
+		// Show border
+        selectionBorder.GetComponent<Renderer>().enabled = true;
+	}
+
+	public void UnselectCard() {
+		this.isSelected = false;
+	
+		// Hide border
+        selectionBorder.GetComponent<Renderer>().enabled = false;
+	}
+
+	public void MoveTowardsCard(CardScript cardToAttack) {
+		this.targetPosition = cardToAttack.transform.position;
+	}
+
+	public void MoveBackToOriginalPosition() {
+		this.targetPosition = originalPosition;
+	}
+
+	public void LoseHealth(int value) {
+		this.healthValue -= value;
+		this.ShowDamageBeingTaken(value);
+	}
+
+	public void PutInAttackLayer() {
+		this.gameObject.layer = 1;
+		this.gameObject.GetComponent<Renderer>().sortingOrder = 1;
+		this.damageIndicator.GetComponent<Renderer>().sortingOrder = 2;
+		this.canvas.sortingOrder = 3;
+	}
+
+	public void RemoveFromAttackLayer() {
+		this.gameObject.layer = 0;
+		this.gameObject.GetComponent<Renderer>().sortingOrder = 0;
+		this.damageIndicator.GetComponent<Renderer>().sortingOrder = 1;
+		this.canvas.sortingOrder = 2;
+	}
+
+	public void HideDamageBeingTaken() {
+		// TODO: lindach: Make damage fade out
+		this.damageIndicator.GetComponent<Renderer>().enabled = false;
+		this.damageValueDisplay.text = "";
+	}
+
+	private void ShowDamageBeingTaken(int damage) {
+		this.damageIndicator.GetComponent<Renderer>().enabled = true;
+		this.damageValueDisplay.text = "-" + damage;
+	}
+
+	private void closeFullCard() {
+		Debug.Log("Closing fulll card");
+		this.fullCard.GetComponent<Renderer>().enabled = false;
+		this.fullCardShowStartTime = default(DateTime);
+	}
+
+	private void startShowFullCard() {
+		Debug.Log("Start showing full card");
+		this.fullCard.GetComponent<Renderer>().enabled = true;
+		this.fullCard.transform.localScale = this.showFullCardAnimationInitialScale;
+		this.fullCardShowStartTime = DateTime.Now;
+	}
+}
---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+	// Time in seconds to wait before cleaning up card after attack
+	private const float cardCleanupDelaySec = 0.75f;
+
+	GameObject attackingCard = null;
+	GameObject defendingCard = null;
+	CameraShakeScript camShakeScript;
+	
+	// Currently always player's turn
+	private bool isPlayersTurn = true;
+
+	public bool IsPlayersTurn {
+		get { return this.isPlayersTurn; }
+	}
+
+	public bool IsAttackInProgress {
+		get { return this.defendingCard != null; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		this.camShakeScript = GetComponent<CameraShakeScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnCardClicked(GameObject cardClicked) {
+		if (this.defendingCard != null) {
+			// Current attack going on
+			return;
+		}
+		
+		CardScript selecedPlayerCardScript = null;
+		CardScript cardClickedScript = cardClicked.GetComponent<CardScript>();
+		if (this.attackingCard != null) {
+			selecedPlayerCardScript = this.attackingCard.GetComponent<CardScript>();
+		}
+
+		// No current selection
+		if (this.attackingCard == null) {
+			if (cardClickedScript.IsEnemyPlayer) {
+				// Selecting enemy card not allowed
+				return;
+			}
+
+			// Selecting player card
+			this.SelectPlayerCard(cardClicked);
+		} else {
+			// We have existing selection on player card
+
+			if (cardClicked == this.attackingCard) {
+				// Selecting same card, unselect
+				this.UnselectPlayerCard(this.attackingCard);
+				this.attackingCard = null;
+			} else if (!cardClickedScript.IsEnemyPlayer) {
+				// We're clicking on another player card
+				// Unselect existing player card
+				this.UnselectPlayerCard(this.attackingCard);
+				this.SelectPlayerCard(cardClicked);
+			} else {
+				// We're clicking on enemy card, attack
+				selecedPlayerCardScript.MoveTowardsCard(cardClickedScript);
+				this.defendingCard = cardClicked;
+
+				// Move cards clashing to attack layer
+				selecedPlayerCardScript.PutInAttackLayer();
+				cardClickedScript.PutInAttackLayer();
+			}
+		}
+	}
+
+	public void OnCardsCollide() {
+
+		// Move attacking card back to original position
+		CardScript attackCardScript = this.attackingCard.GetComponent<CardScript>();
+		attackCardScript.MoveBackToOriginalPosition();
+
+		// Unselect attacking card
+		this.UnselectPlayerCard(this.attackingCard);
+
+		// Update health values
+		CardScript defendingCardScript = this.defendingCard.GetComponent<CardScript>();
+		attackCardScript.LoseHealth(defendingCardScript.AttackValue);
+		defendingCardScript.LoseHealth(attackCardScript.AttackValue);
+
+		// Shake camera
+		camShakeScript.Shake();
+	}
+
+	public void onAttackFinished() {
+		// Put cards back to default layer
+		CardScript defendingCardScript = this.defendingCard.GetComponent<CardScript>();
+		defendingCardScript.RemoveFromAttackLayer();
+		
+		CardScript attackCardScript = this.attackingCard.GetComponent<CardScript>();
+		attackCardScript.RemoveFromAttackLayer();
+
+		// Do clean up of attack on delay
+		StartCoroutine(CleanUpAttackOnDelay(attackCardScript, defendingCardScript));
+
+		// Attack has finished
+		this.defendingCard = null;
+		this.attackingCard = null;
+	}
+
+	private IEnumerator CleanUpAttackOnDelay(CardScript attackCardScript, CardScript defendingCardScript) {
+		yield return new WaitForSeconds(GameManagerScript.cardCleanupDelaySec);
+
+		// Hide damage taken on delay
+		defendingCardScript.HideDamageBeingTaken();
+		attackCardScript.HideDamageBeingTaken();
+
+		// TODO: lindach: Animate death
+		// Check if cards have been destroyed
+		if (attackCardScript.HealthValue <= 0) {
+			Destroy(attackCardScript.gameObject);
+		}
+
+		if (defendingCardScript.HealthValue <= 0) {
+			Destroy(defendingCardScript.gameObject);
+		}
+
+		
+	}
+
+	private void SelectPlayerCard(GameObject playerCard) {
+		playerCard.GetComponent<CardScript>().SelectCard();
+		this.attackingCard = playerCard;
+	}
+
+	private void UnselectPlayerCard(GameObject playerCard) {
+		playerCard.GetComponent<CardScript>().UnselectCard();
+	}
+}
